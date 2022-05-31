3ebef384c556e0f8234dbda754566015c3259ba9
stijn <stijn.vannieuwpoort@student.hu.nl>
2022-05-31 21:19:16 +0200

    domeinlaag beta

--- src/main/java/domeinlaag/Categorie.java
+++ src/main/java/domeinlaag/Categorie.java
@@ -1,5 +1,29 @@
 package main.java.domeinlaag;
 
+import java.util.ArrayList;
+
 public class Categorie {
     private String naam;
+    private ArrayList<Recept> deRecepten = new ArrayList<>();
+
+    public Categorie (String nm) {
+        this.setNaam(nm);
+    }
+
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public ArrayList<Recept> getDeRecepten() {
+        return deRecepten;
+    }
+
+    public void setDeRecepten(ArrayList<Recept> deRecepten) {
+        this.deRecepten = deRecepten;
+    }
 }
